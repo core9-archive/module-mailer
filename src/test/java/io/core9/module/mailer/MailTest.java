7e9713e5154f60ae39be44f209004b84a566386c
Robert Cooke <robert.cooke@trimm.nl>
2014-04-16 15:35:20 +0200

    init

---
+++ src/test/java/io/core9/module/mailer/MailTest.java
@@ -0,0 +1,14 @@
+package io.core9.module.mailer;
+
+
+
+import org.junit.Test;
+
+public class MailTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
---
+++ src/test/java/io/core9/plugin/nashorn/sample/NashornTest2.js
@@ -0,0 +1,12 @@
+if (typeof ph == 'undefined') {
+	var ph = 10;
+}
+
+
+
+
+
+function sum2(a, b) {
+
+	return a + b + ph;
+}
